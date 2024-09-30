f1a575876cbedb8ec88d010ad8b75296ee86049a
HXLuong <123tutuni@gmail.com>
2024-09-30 14:06:35 +0700

    first commit

---
+++ src/main/java/com/claz/ClazApplication.java
@@ -0,0 +1,13 @@
+package com.claz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClazApplication.class, args);
+	}
+
+}
