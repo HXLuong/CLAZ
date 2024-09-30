bf8c930c564fdc316a8a90fd94696fcd2fa09f7b
Thanhanz2 <thanhannguyen415@gmail.com>
2024-09-30 14:54:19 +0700

    danh muc frontend

--- src/main/java/com/claz/controllers/HomeController.java
+++ src/main/java/com/claz/controllers/HomeController.java
@@ -10,4 +10,8 @@ public class HomeController {
     public String index(Model model) {
         return "index";
     }
+	@RequestMapping("/danhmuc")
+    public String danhmuc(Model model) {
+        return "danhmuc";
+    }
 }
