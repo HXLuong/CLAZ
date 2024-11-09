b4b46c634c80a60b3c0ae54c8fabc4b99dbb851f
chienlm30072004 <lec99581@gmail.com>
2024-11-09 15:05:25 +0700

    chien_push

--- src/main/java/com/claz/controllers/ChatController.java
+++ src/main/java/com/claz/controllers/ChatController.java
@@ -1,29 +1,29 @@
-package com.claz.controllers;
-
-import com.claz.models.ChatHistory;
-import com.claz.services.ChatService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.List;
-
-@RestController
-@RequestMapping("/chat")
-public class ChatController {
-
-    private final ChatService chatService;
-
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
-
-    @PostMapping("/send")
-    public String sendMessage(@RequestParam String userId, @RequestParam String message) {
-        return chatService.getResponse(message, userId);
-    }
-
-    @GetMapping("/history/{userId}")
-    public List<ChatHistory> getChatHistory(@PathVariable String userId) {
-        return chatService.getChatHistory(userId).orElse(Collections.emptyList());
-    }
-}
+//package com.claz.controllers;
+//
+//import com.claz.models.ChatHistory;
+//import com.claz.services.ChatService;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/chat")
+//public class ChatController {
+//
+//    private final ChatService chatService;
+//
+//    public ChatController(ChatService chatService) {
+//        this.chatService = chatService;
+//    }
+//
+//    @PostMapping("/send")
+//    public String sendMessage(@RequestParam String userId, @RequestParam String message) {
+//        return chatService.getResponse(message, userId);
+//    }
+//
+//    @GetMapping("/history/{userId}")
+//    public List<ChatHistory> getChatHistory(@PathVariable String userId) {
+//        return chatService.getChatHistory(userId).orElse(Collections.emptyList());
+//    }
+//}
